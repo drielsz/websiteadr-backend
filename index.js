ab1eb43a61df4d834e50ee5ed36c7ea0aee20490
drielsz <adrielllxavi@outlook.com>
2024-11-07 10:31:23 -0300

    cors

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+
+
+
 const app = express();
+const port = 3000;
 const appCustom = require('./config/appCustom');
 
-const port = 3000
 
+const corsOptions = {
+    origin: ['http://127.0.0.1:5500', 'https://websiteadr-backend-production.up.railway.app', 'https://www.adrielwebdesign.site'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true,  
+    optionsSuccessStatus: 204 
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 appCustom(app, express);
 
+
 app.listen(port, () => {
-    console.log(`Server runing on port ${port}`);
-})
+    console.log(`Server running on port ${port}`);
+});
